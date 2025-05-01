ddf537e129a967510abc84f4fdad7beed7a1a707
IzTran05 <izabellatran@gmail.com>
2025-05-01 11:46:33 +1000

    Light Animator And Event System
    Added light animation with event system

---
+++ Assets/Scripts/Managers/LightEvents.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class LightEvents : MonoBehaviour
+{
+
+    public void NightOver()
+    {
+        print("Night Over.");
+        //trigger game over screen 
+
+    }
+
+}
